916806ca51b3108262b84a2a3a789c88df376a83
myous <myousef049@gmail.com>
2022-12-21 15:52:37 +0200

    Add project files.

---
+++ Login-App/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Login_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text.Json.Serialization;
+
+namespace Login_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        Users user;
+        List<Users> result;
+        Uri BASE_URL = new Uri("https://localhost:7228/");
+        HttpClient client;
+        HttpResponseMessage responseMessage;
+
+        public HomeController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = BASE_URL;
+            responseMessage = client.GetAsync(BASE_URL + "api/users").Result;
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                string data = responseMessage.Content.ReadAsStringAsync().Result;
+                result = JsonConvert.DeserializeObject<List<Users>>(data);
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login(string userName, string password)
+        {
+            foreach (var item in result)
+            {
+                if (item.UserName.Equals(userName) && item.Password.Equals(password))
+                {
+                    return View(item);
+                }
+            }
+            return View("NotFound");
+        }
+
+    }
+}
---
+++ Login-App/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Login_App.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+        
+        [Required]
+        public string Password { get; set; }
+    }
+}
